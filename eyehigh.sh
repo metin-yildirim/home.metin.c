1751d387ff4217fd17a23453fa1196f27463f592
Metin Yıldırım <60541566+metin-yildirim@users.noreply.github.com>
2020-07-02 01:08:53 +0300

    01 Temmuz 2020 ilk kez eklendi.

---
+++ eyehigh.sh
@@ -0,0 +1,1 @@
+brightness-set 0.7
---
+++ initc.sh
@@ -0,0 +1,6 @@
+sudo ln -s /home/metin/c/brightness-set.sh /usr/bin/brightness-set
+sudo ln -s /home/metin/c/gamma-set.sh /usr/bin/gamma-set
+sudo ln -s /home/metin/c/eyehigh.sh /usr/bin/eyehigh
+sudo ln -s /home/metin/c/eyelow.sh /usr/bin/eyelow
+sudo ln -s /home/metin/c/batterynow.sh /usr/bin/batterynow
+chmod +x /home/metin/c/*
